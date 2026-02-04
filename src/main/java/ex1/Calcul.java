b66b611126ae270012b9f5d4a1218518806991db
Kyldof <kyllian.ronne@laposte.net>
2026-02-04 14:10:25 +0100

    ex1

---
+++ src/main/java/ex1/Calcul.java
@@ -0,0 +1,27 @@
+package ex1;
+
+/**
+ * Classe permettant contenant plusieurs fonctions permmettant du calcul mathématique
+ */
+public class Calcul {
+
+    /**
+     * fonction retournant l'addition de a + b
+     * @param a
+     * @param b
+     * @return
+     */
+    public int addition(int a, int b) {
+        return a + b;
+    }
+
+    /**
+     * fonction retournant la soustraction de a - b
+     * @param a
+     * @param b
+     * @return
+     */
+    public int soustraction(int a, int b) {
+        return a - b;
+    }
+}
