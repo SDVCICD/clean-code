63b0fe1c7f4f02a245c66e7bdc73f3a53f2646c8
Kyldof <kyllian.ronne@laposte.net>
2026-02-04 14:13:25 +0100

    ex1

---
+++ src/main/java/ex1/Entreprise.java
@@ -0,0 +1,17 @@
+package ex1;
+
+import java.util.Date;
+
+public class Entreprise {
+
+    public static final int CAPITAL_MAX = 3000000;
+    private int siret;
+    private String nom;
+    private String adresse;
+    private Date dateCreation;
+
+    public void afficherStatut() {
+
+    }
+
+}
